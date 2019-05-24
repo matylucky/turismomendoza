3a8477b1d65e7d0b04feec00462df549a2934b6c
matylucky <50973615+matylucky@users.noreply.github.com>
2019-05-23 23:23:15 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>Turismo Mendoza</title>
- </head>
- <body>
- <?php echo '<p>Bienvenidos a la pagina de turismomendoza</p>'; ?>
- <?php echo '<p>Esto es un texto de prueba</p>'; ?>
- </body>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <title>Bievenidos</title>
+    <meta name="description" content="Thoughts, stories and ideas." />
+
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta property="og:site_name" content="Turismo Mendoza" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Turismo Mendoza" />
+    <meta property="og:description" content="Thoughts, stories and ideas." />
+    <meta property="og:url" content="https://turismomendoza.heroku.com/" />
+    
+
+
+</main>
+
+
+        <footer class="site-footer clearfix">
+            <section class="copyright"><a href="http://blog.lolagomezdebarros.es">Lola Blog</a> &copy; 2019</section>
+            <section class="poweredby">Proudly published with <a href="https://ghost.org">Ghost</a></section>
+        </footer>
+
+    </div>
+
+</body>
 </html>
