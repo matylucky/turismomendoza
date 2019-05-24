7adfa2253669d95294d91036252da4e1415a5752
matylucky <50973615+matylucky@users.noreply.github.com>
2019-05-23 23:39:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,167 +4,18 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <title>Bievenidos</title>
+    <title>Bievenidos a Mendoza Turismo</title>
     <meta name="description" content="Thoughts, stories and ideas." />
 
     <meta name="HandheldFriendly" content="True" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="shortcut icon" href="/favicon.ico">
-
-    <link rel="stylesheet" type="text/css" href="/assets/css/screen.css?v=134d9a8098" />
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:700,400" />
-
-    <link rel="canonical" href="http://blog.lolagomezdebarros.es/" />
-    <meta name="referrer" content="origin" />
-    <link rel="next" href="https://blog.lolagomezdebarros.es/page/2/" />
-    
-    <meta property="og:site_name" content="Lola Blog" />
+    <meta property="og:site_name" content="Mendoza Turismo" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Lola Blog" />
+    <meta property="og:title" content="Mendoza turismo" />
     <meta property="og:description" content="Thoughts, stories and ideas." />
-    <meta property="og:url" content="http://blog.lolagomezdebarros.es/" />
+    <meta property="og:url" content="https://mendozatursimo.herokuapp.com/" />
     
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content="Lola Blog" />
-    <meta name="twitter:description" content="Thoughts, stories and ideas." />
-    <meta name="twitter:url" content="http://blog.lolagomezdebarros.es/" />
-    
-    <script type="application/ld+json">
-{
-    "@context": "http://schema.org",
-    "@type": "Website",
-    "publisher": "Lola Blog",
-    "url": "https://blog.lolagomezdebarros.es/",
-    "description": "Thoughts, stories and ideas."
-}
-    </script>
-
-    <meta name="generator" content="Ghost 0.7" />
-    <link rel="alternate" type="application/rss+xml" title="Lola Blog" href="https://blog.lolagomezdebarros.es/rss/" />
-</head>
-<body class="home-template nav-closed">
-
-    <div class="nav">
-    <h3 class="nav-title">Menu</h3>
-    <a href="#" class="nav-close">
-        <span class="hidden">Close</span>
-    </a>
-    <ul>
-            <li class="nav-home nav-current" role="presentation"><a href="https://blog.lolagomezdebarros.es/">Home</a></li>
-    </ul>
-    <a class="subscribe-button icon-feed" href="http://blog.lolagomezdebarros.es/rss/">Subscribe</a>
-</div>
-<span class="nav-cover"></span>
-
-
-    <div class="site-wrapper">
-
-        
-<header class="main-header no-cover">
-    <nav class="main-nav overlay clearfix">
-        
-            <a class="menu-button icon-menu" href="#"><span class="word">Menu</span></a>
-    </nav>
-    <div class="vertical">
-        <div class="main-header-content inner">
-            <h1 class="page-title">Lola Blog</h1>
-            <h2 class="page-description">Thoughts, stories and ideas.</h2>
-        </div>
-    </div>
-    <a class="scroll-down icon-arrow-left" href="#content" data-offset="-45"><span class="hidden">Scroll Down</span></a>
-</header>
-
-<main id="content" class="content" role="main">
-
-    <div class="extra-pagination inner">
-    <nav class="pagination" role="navigation">
-    <span class="page-number">Page 1 of 3</span>
-        <a class="older-posts" href="/page/2/">Older Posts <span aria-hidden="true">&rarr;</span></a>
-</nav>
-
-</div>
-
-<article class="post tag-relatos">
-    <header class="post-header">
-        <h2 class="post-title"><a href="/el-negocio/">1El negocio</a></h2>
-    </header>
-    <section class="post-excerpt">
-        
-        <p>Estoy sorprendido de seguir comportándome con mi hermano como cuando éramos pequeños, unidos como siameses aunque solo seamos gemelos, y ya tenemos 40 años. Entonces él <a class="read-more" href="/el-negocio/">&raquo;</a></p>
-    </section>
-    <footer class="post-meta">
-        
-        <a href="/author/lola-2/">lola</a>
-         en <a href="/tag/relatos/">relatos</a>
-        <time class="post-date" datetime="2016-03-22">22 March 2016</time>
-    </footer>
-</article>
-<article class="post tag-fotografia">
-    <header class="post-header">
-        <h2 class="post-title"><a href="/10-fotografia-y-narracion/">#10 Fotografía y narración</a></h2>
-    </header>
-    <section class="post-excerpt">
-        <span><img src=https://dl.dropboxusercontent.com/s/cmk7s9857g8qx3i/foto%20LA%20TORRE%2020160319_190648.jpg?dl=0 /></span>
-        <p>Foto de J.Julio Ruiz Benavides. La torre en la colina se hacía visible desde la carretera cinco kilómetros antes de llegar al pueblo, en otro <a class="read-more" href="/10-fotografia-y-narracion/">&raquo;</a></p>
-    </section>
-    <footer class="post-meta">
-        
-        <a href="/author/lola-2/">lola</a>
-         en <a href="/tag/fotografia/">Fotografía</a>
-        <time class="post-date" datetime="2016-03-22">22 March 2016</time>
-    </footer>
-</article>
-<article class="post tag-fotografia">
-    <header class="post-header">
-        <h2 class="post-title"><a href="/untitled-2/">#9 Fotografía y narración</a></h2>
-    </header>
-    <section class="post-excerpt">
-        
-        <p>Foto de Robert Doisneau No podía hacer el esfuerzo de salir fuera de la barraca e ir a la mina de oro a trabajar, la enfermedad <a class="read-more" href="/untitled-2/">&raquo;</a></p>
-    </section>
-    <footer class="post-meta">
-        
-        <a href="/author/lola-2/">lola</a>
-         en <a href="/tag/fotografia/">Fotografía</a>
-        <time class="post-date" datetime="2016-03-22">22 March 2016</time>
-    </footer>
-</article>
-<article class="post tag-fotografia">
-    <header class="post-header">
-        <h2 class="post-title"><a href="/8-fotografia-y-narracion/">#8 Fotografía y narración</a></h2>
-    </header>
-    <section class="post-excerpt">
-        
-        <p>Foto de Leszek Bujnowski Ray envió la fotografía, con una nota pidiéndome un escueto informe que dictaminara la autoría de quien firmaba en el reverso. Una <a class="read-more" href="/8-fotografia-y-narracion/">&raquo;</a></p>
-    </section>
-    <footer class="post-meta">
-        
-        <a href="/author/lola-2/">lola</a>
-         en <a href="/tag/fotografia/">Fotografía</a>
-        <time class="post-date" datetime="2016-03-22">22 March 2016</time>
-    </footer>
-</article>
-<article class="post tag-fotografia">
-    <header class="post-header">
-        <h2 class="post-title"><a href="/untitled/">#3 Fotografía y narración</a></h2>
-    </header>
-    <section class="post-excerpt">
-        
-        <p>Foto de Stevenson Odio esta casa, odio este campo, odio los animales,  odio a mi familia. No volveré a salir fuera.  Me voy a quedar aquí, <a class="read-more" href="/untitled/">&raquo;</a></p>
-    </section>
-    <footer class="post-meta">
-        
-        <a href="/author/lola-2/">lola</a>
-         en <a href="/tag/fotografia/">Fotografía</a>
-        <time class="post-date" datetime="2016-03-22">22 March 2016</time>
-    </footer>
-</article>
-
-<nav class="pagination" role="navigation">
-    <span class="page-number">Page 1 of 3</span>
-        <a class="older-posts" href="/page/2/">Older Posts <span aria-hidden="true">&rarr;</span></a>
-</nav>
 
 
 </main>
