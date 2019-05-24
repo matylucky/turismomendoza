617bf8382b8b6f2730c5dca097ea59a35d3b5153
matylucky <50973615+matylucky@users.noreply.github.com>
2019-05-23 22:53:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
  <head>
-  <title>Prueba de PHP</title>
+  <title>Turismo Mendoza</title>
  </head>
  <body>
  <?php echo '<p>Bienvenidos a la pagina de turismomendoza</p>'; ?>
+ <?php echo '<p>Esto es un texto de prueba</p>'; ?>
  </body>
 </html>
