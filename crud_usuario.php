d5c2b31a78e53a9fb3e5d2efd8f293d5b225b19a
matylucky <50973615+matylucky@users.noreply.github.com>
2019-06-20 18:30:53 -0300

    Update crud_usuario.php

--- crud_usuario.php
+++ crud_usuario.php
@@ -7,10 +7,10 @@
 		public function __construct(){}
 
 		//inserta los datos del usuario
-		public function insertar($USU_EMAIL){
+		public function insertar($usuario){
 			$db=DB::conectar();
 			$insert=$db->prepare('INSERT INTO usuarios VALUES(NULL,:USU_EMAIL, :USU_CONTRASEÑA)');
-			$insert->bindValue('USU_EMAIL',$USU_EMAIL->getNombre());
+			$insert->bindValue('USU_EMAIL',$usuario->getNombre());
 			//encripta la clave
 			$pass=password_hash($usuario->getClave(),PASSWORD_DEFAULT);
 			$insert->bindValue('clave',$pass);
@@ -24,7 +24,7 @@
 			$select->bindValue('USU_EMAIL',$USU_EMAIL);
 			$select->execute();
 			$registro=$select->fetch();
-			$USU_EMAIL=new USU_EMAIL();
+			$USU_EMAIL=new usuario();
 			//verifica si la clave es conrrecta
 			if (password_verify($USU_CONTRASENA, $registro['USU_CONTRASEÑA'])) {				
 				//si es correcta, asigna los valores que trae desde la base de datos
@@ -32,7 +32,7 @@
 				$usuario->setNombre($registro['USU_EMAIL']);
 				$usuario->setClave($registro['USU_CONTRASEÑA']);
 			}			
-			return $USU_EMAIL;
+			return $usuario;
 		}
 
 		//busca el nombre del usuario si existe
