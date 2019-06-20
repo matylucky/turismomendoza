9f50c7a86c90b86ddc97d96a028c9129d5f6345f
matylucky <50973615+matylucky@users.noreply.github.com>
2019-06-20 18:41:27 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@
 
 		public static function conectar(){
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion=new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_06e2145fb0a0577','bdaacf63d00d60','565681cb',$pdo_options);
+			self::$conexion=new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_06e2145fb0a0577','565681cb','bdaacf63d00d60',$pdo_options);
 			return self::$conexion;
 		}
 	}
