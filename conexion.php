c09c3a6103557e29277fd15eff0144ace3f60e14
matylucky <50973615+matylucky@users.noreply.github.com>
2019-06-20 19:08:27 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -15,11 +15,11 @@
 		// Create connection
 		$conexion = mysqli_connect($servername, $username, $password, $database);
 		// Check connection
-		if (!$conexion) {
- 		   die("Connection failed: " . mysqli_connect_error());
-		}
-		echo "Connected successfully";
-		mysqli_close($conexion);
+		//if (!$conexion) {
+ 		//   die("Connection failed: " . mysqli_connect_error());
+		//}
+		//echo "Connected successfully";
+		//mysqli_close($conexion);
 		}
 	}
 ?>
