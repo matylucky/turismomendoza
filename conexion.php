0b32fa4d76cbcf3f29cf836e4db235b3a9e4cd48
matylucky <50973615+matylucky@users.noreply.github.com>
2019-06-20 19:06:37 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -7,7 +7,7 @@
 		//	$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
 		//	self::$conexion=new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_06e2145fb0a0577','565681cb','bdaacf63d00d60',$pdo_options);
 		//	return self::$conexion;
-		
+		public static function conectar(){
 		$servername = "us-cdbr-iron-east-02.cleardb.net";
 		$database = "heroku_06e2145fb0a0577";
 		$username = "bdaacf63d00d60";
