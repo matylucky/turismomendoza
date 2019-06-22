60bfe2122243e1107610067fae1075481f055450
matylucky <50973615+matylucky@users.noreply.github.com>
2019-06-22 11:46:01 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,24 +3,25 @@
 		private static $conn=null;
 		private function __construct(){}
 
-		//public static function conectar(){
-		//	$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-		//	self::$conexion=new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_06e2145fb0a0577','565681cb','bdaacf63d00d60',$pdo_options);
-		//	return self::$conexion;
 		public static function conectar(){
+			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
+			self::$conexion=new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_06e2145fb0a0577','565681cb','bdaacf63d00d60',$pdo_options);
+			return self::$conexion;
+			echo "Conectado";
+		//public static function conectar(){
 		//$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-		$servername = "us-cdbr-iron-east-02.cleardb.net";
-		$database = "heroku_06e2145fb0a0577";
-		$username = "bdaacf63d00d60";
-		$password = "565681cb";
+		//$servername = "us-cdbr-iron-east-02.cleardb.net";
+		//$database = "heroku_06e2145fb0a0577";
+		//$username = "bdaacf63d00d60";
+		//$password = "565681cb";
 		//Create connection
-		$conn = mysqli_connect($servername, $username, $password, $database);
+		//$conn = mysqli_connect($servername, $username, $password, $database);
 		// Check connection
-		if (!$conn) {
-  		  die("Connection failed: " . mysqli_connect_error());
-		}
-		echo "Conectado";
-		mysqli_close($conn);
+		//if (!$conn) {
+  		//  die("Connection failed: " . mysqli_connect_error());
+		//}
+		//echo "Conectado";
+		//mysqli_close($conn);
 		}
 	}
 ?>
