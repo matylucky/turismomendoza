b57986fd65fbccdd9906ad3f4654fac4025bef7a
matylucky <50973615+matylucky@users.noreply.github.com>
2019-07-05 19:08:49 -0300

    Update login.php

--- login.php
+++ login.php
@@ -8,32 +8,32 @@
 	//inicio de sesion
 	session_start();
 
-	$usuario=new Usuario();
+	$USU_EMAIL=new Usuario();
 	$crud=new CrudUsuario();
 	//verifica si la variable registrarse está definida
 	//se da que está definicda cuando el usuario se loguea, ya que la envía en la petición
 	if (isset($_POST['registrarse'])) {
-		$usuario->setNombre($_POST['usuario']);
-		$usuario->setClave($_POST['pas']);
-		if ($crud->buscarUsuario($_POST['usuario'])) {
-			$crud->insertar($usuario);
+		$USU_EMAIL->setUSU_EMAIL($_POST['USU_EMAIL']);
+		$USU_EMAIL->setUSU_PASS($_POST['USU_PASS']);
+		if ($crud->buscarUsuario($_POST['USU_EMAIL'])) {
+			$crud->insertar($USU_EMAIL);
 			header('Location: index.php');
 		}else{
 			header('Location: error.php?mensaje=El nombre de usuario ya existe');
 		}		
 		
 	}elseif (isset($_POST['entrar'])) { //verifica si la variable entrar está definida
-		$usuario=$crud->obtenerUsuario($_POST['usuario'],$_POST['pas']);
+		$usuario=$crud->obtenerUsuario($_POST['USU_EMAIL'],$_POST['USU_PASS']);
 		// si el id del objeto retornado no es null, quiere decir que encontro un registro en la base
 		if ($usuario->getId()!=NULL) {
-			$_SESSION['usuario']=$usuario; //si el usuario se encuentra, crea la sesión de usuario
+			$_SESSION['USU_EMAIL']=$USU_EMAIL; //si el usuario se encuentra, crea la sesión de usuario
 			header('Location: cuenta.php'); //envia a la página que simula la cuenta
 		}else{
 			header('Location: error.php?mensaje=Tus nombre de usuario o clave son incorrectos'); // cuando los datos son incorrectos envia a la página de error
 		}
 	}elseif(isset($_POST['salir'])){ // cuando presiona el botòn salir
 		header('Location: index.php');
-		unset($_SESSION['usuario']); //destruye la sesión
+		unset($_SESSION['USU_EMAIL']); //destruye la sesión
 	}
 ?>
 
