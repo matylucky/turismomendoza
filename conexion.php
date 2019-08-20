90ea9bf16d9c82910d74a153fc895b61c5572861
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-20 00:06:42 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,5 +5,5 @@ $host_db = "us-cdbr-iron-east-02.cleardb.net";
  $user_db = "bdaacf63d00d60";
  $pass_db = "565681cb";
  $db_name = "heroku_06e2145fb0a0577";
- $tbl_name = "usuariosdb";
+ $tbl_name = "usuario";
 ?>
