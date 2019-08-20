974b47cdb48ebb995693c2d5d34783573d16e00c
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-19 23:13:39 -0300

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,27 +1,9 @@
-<?php 
-	class Db{
-		private static $conn=null;
-		private function __construct(){}
-
-		public static function conectar(){
-			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion=new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_06e2145fb0a0577','565681cb','bdaacf63d00d60',$pdo_options);
-			return self::$conexion;
-			echo "Conectado";
-		//public static function conectar(){
-		//$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-		//$servername = "us-cdbr-iron-east-02.cleardb.net";
-		//$database = "heroku_06e2145fb0a0577";
-		//$username = "bdaacf63d00d60";
-		//$password = "565681cb";
-		//Create connection
-		//$conn = mysqli_connect($servername, $username, $password, $database);
-		// Check connection
-		//if (!$conn) {
-  		//  die("Connection failed: " . mysqli_connect_error());
-		//}
-		//echo "Conectado";
-		//mysqli_close($conn);
-		}
-	}
+<!-- ingresa todos los datos referentes a tu conexion de base de datos
+de esta forma solo tendras que modificar un solo archivo -->
+<?php
+$host_db = "us-cdbr-iron-east-02.cleardb.net";
+ $user_db = "bdaacf63d00d60";
+ $pass_db = "565681cb";
+ $db_name = "heroku_06e2145fb0a0577";
+ $tbl_name = "usuariosdb";
 ?>
