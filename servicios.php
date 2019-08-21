48ef094e6675ec962b450671a248ba41342950a1
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-20 23:51:58 -0300

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -26,22 +26,22 @@
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
+      <!--  <span class="icon-bar"></span> -->
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>    
-		<span class="icon-bar"></span> 		
+	<span class="icon-bar"></span> 		
       </button>
       <a class="navbar-brand" href="#">Turismo Mendoza</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li><a href="testcarr.php">Inicio</a></li>
-        <li><a href="#">Reservas</a></li>
+        <li><a href="index.php">Inicio</a></li>
+        <!--<li><a href="#">Reservas</a></li>-->
         <li><a href="#">Nosotros</a></li>
         <li><a href="contacto.php">Contacto</a></li>
-		<li class="active"><a href="#">Servicios</a></li>
+	<li class="active"><a href="#">Servicios</a></li>
       </ul>
-             </button> <a class="navbar-brand navbar-right" href="index.php"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a>
+             </button> <a class="navbar-brand navbar-right" href="logeo.php"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a>
       <!--<ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a><onclick="location.href='index.html'"></li>
       </ul> -->
