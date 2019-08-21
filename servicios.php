ea2bf750e17a9cb7ee7c69b9be4dcbcbcc5eba01
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-20 22:44:06 -0300

    Add files via upload

---
+++ servicios.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Servicios</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>    
+		<span class="icon-bar"></span> 		
+      </button>
+      <a class="navbar-brand" href="#">Turismo Mendoza</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="testcarr.php">Inicio</a></li>
+        <li><a href="#">Reservas</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
+		<li class="active"><a href="#">Servicios</a></li>
+      </ul>
+             </button> <a class="navbar-brand navbar-right" href="index.php"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a>
+      <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a><onclick="location.href='index.html'"></li>
+      </ul> -->
+    </div>
+  </div>
+</nav>
+
+<h3>Servicios</h3>
+<img src="https://i.ibb.co/RQL4C6H/Bus-Mercedes-Benz-Sprinter.jpg" alt="Van">
+<h3>Horarios:</h3>
+<h3>Lunes a Viernes, salidas a las 8hs y regreso a las 17hs</h3>
+<h3>Sabados y Domginso, salidas a las 7hs y regreso a las 16hs</h3>
+
+
+</body>
+</html>
