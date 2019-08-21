04240b27115d4357613e3c458373b584ce798d7b
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-20 21:37:20 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -12,6 +12,6 @@ session_start();
 unset ($SESSION['username']);
 session_destroy();
 
-header('Location: https://turismomendoza.herokuapp.com/index.php');
+header('Location: https://turismomendoza.herokuapp.com/testcarr.php');
 
 ?>
