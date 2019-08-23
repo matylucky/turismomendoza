28547bcbef3c01268fb1c813f3b7a31461e4e686
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-23 19:48:14 -0300

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -50,7 +50,7 @@ if ($password==$row['USU_PASS']) {
     //echo "<br><br><a href=panel-control.php>Panel de Control</a>"; 
     //header('Location: https://turismomendoza.herokuapp.com/panel-control.php');//redirecciona a la pagina del usuario
     header('Location: https://turismomendoza.herokuapp.com/index2.php');//redirecciona a la pagina del usuario
-	    }
+//	    }
 //	    else{ $_SESSION['loggedin'] = true;
   // 			$_SESSION['username'] = $username;
 //			$_SESSION['usuario'] = $row['USU_NOMBRE'];
