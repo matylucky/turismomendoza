f1eaf92984652898e39f6b37cbc86bd230f02709
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-28 08:42:47 -0300

    Add files via upload

---
+++ destino1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Destino 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<h3>Destino 1</h3>
+<img src="bird.jpg" alt="Bird">
+<h3>Esto es una prueba</h3>
+
+</body>
+</html>
