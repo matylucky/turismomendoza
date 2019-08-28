a58de8800807a6befbfde633293f5ab746db2e4b
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-28 08:46:26 -0300

    Create destino2.php

---
+++ destino2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Destino 2</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<h3>Destino 2</h3>
+<img src="bird.jpg" alt="Bird">
+<h3>Esto es una prueba de destino 2</h3>
+
+</body>
+</html>
