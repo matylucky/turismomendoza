9346f24989444c9236c74b2eba36393ed9718d8c
matylucky <50973615+matylucky@users.noreply.github.com>
2019-08-29 09:00:31 -0300

    Update registropasajero.php

--- registropasajero.php
+++ registropasajero.php
@@ -5,16 +5,91 @@
                             e.preventDefault();
                     }
 </script>
+<?php
+session_start();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+ 
+ 
+ 
+} else {
+   echo "Inicia Sesion para acceder a este contenido.<br>";
+   echo "<br><a href='login.html'>Login</a>";
+   echo "<br><br><a href='index.html'>Registrarme</a>";
+   header('Location: https://turismomendoza.herokuapp.com/login.html');//redirige a la página de login si el usuario quiere ingresar sin iniciar sesion
+exit;
+}
+//$usuarios = $_POST['usuarios'];
+ 
+$now = time();
+if($now > $_SESSION['expire']) {
+session_destroy();
+header('Location: https://turismomendoza.herokuapp.com/login.html');//redirige a la página de login, modifica la url a tu conveniencia
+echo "Tu sesion a expirado,
+<a href='login.html'>Inicia Sesion</a>";
+exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Registro de usuario</title>
+  <title>Registro de Pasajeros</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+    
+  .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none; 
+    }
+  }
+  </style>
 </head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"> <p><?php echo  $_SESSION['usuario'];?></p> </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="#"><a href="#">Inicio</a></li>
+        <li><a href="#">Reservas</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="contacto2.php">Contacto</a></li>
+      </ul>
+             </button> <a class="navbar-brand navbar-right" href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Cerrar Sesión</a>
+      <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a><onclick="location.href='index.html'"></li>
+      </ul> -->
+    </div>
+  </div>
+</nav>
 	
 	
 	
@@ -26,7 +101,7 @@
     <div class="form-group">
       <label class="control-label col-sm-2" for="email">Nombre y Apellido</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" maxlength="20" placeholder="Ingresa tu Nombre y Apellido" name="username" required>
+        <input type="text" class="form-control" maxlength="20" placeholder=<p><?php echo  $_SESSION['usuario'];?></p> name="username" required>
       </div>
     </div>
     <div class="form-group">
