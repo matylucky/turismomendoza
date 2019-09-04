72c8542ec7f322aac85c60238a45f2de4f86709d
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-04 12:39:57 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -6,4 +6,6 @@ $host_db = "us-cdbr-iron-east-02.cleardb.net";
  $pass_db = "565681cb";
  $db_name = "heroku_06e2145fb0a0577";
  $tbl_name = "usuario";
+ $tbl_name2 = "reserva";
+ $tbl_name3 = "pasajeros";
 ?>
