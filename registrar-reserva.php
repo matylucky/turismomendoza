7f0a1d38c41de566d0df8c14f1bf55957ed7e906
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-05 08:49:53 -0300

    Update registrar-reserva.php

--- registrar-reserva.php
+++ registrar-reserva.php
@@ -18,7 +18,7 @@
  }
  else{
 */
- //$query = "INSERT INTO $tbl_name2 (USU_ID, PAQ_ID,) VALUES ('$_POST[email]', '$_POST[paquete2]')";
+ $query = "INSERT INTO $tbl_name2 (USU_ID, PAQ_ID,) VALUES ('$_POST[email]', '$_POST['$paquete2']')";
  $query = "INSERT INTO $tbl_name3 (PAS_NOMBRE, PAS_DNI) VALUES ('$_POST[username]', '$_POST[dni]')";
 
  if ($conexion->query($query) === TRUE) {
