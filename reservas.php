6ae333d03ad161a7ddb122abfafcf109c229fd07
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-06 19:16:01 -0300

    Update reservas.php

--- reservas.php
+++ reservas.php
@@ -1,5 +1,38 @@
 <?php
 include("encabezado2.php"); //incluimos el archivo logotipo.php
+include("conexion.php");
+
+
+$link = mysql_connect($server, $db_user, $db_pass) 
+or die ("Could not connect to mysql because ".mysql_error()); 
+
+// select the database
+mysql_select_db($database) 
+or die ("Could not select database because ".mysql_error()); 
+
+$mail2 = $_POST['mail2'];
+
+$match = "Select * FROM $tbl_name2 WHERE RES_EMAIL = '$mail2'";
+
+$qry = mysql_query($match) 
+or die ("Could not match data because ".mysql_error()); 
+$num_rows = mysql_num_rows($qry); 
+
+if ($num_rows > 0) { 
+    
+        
+    $_SESSION['pasajero'] = $row['PAS_NOMBRE'];
+    $_SESSION['mail2'] = $row['RES_EMAIL'];
+    $_SESSION['dni2'] = $row['PAS_DNI'];
+}
+else{
+echo "Lo Sentimos no tiene reserva registrada"; 
+}
+    exit;
+
+} 
+ob_end_flush();
+
 
 ?>
 
