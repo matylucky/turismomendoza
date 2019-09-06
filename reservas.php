4227d332d3bf273ec11597ab958f72b9d41a9216
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-06 11:07:46 -0300

    Update reservas.php

--- reservas.php
+++ reservas.php
@@ -6,8 +6,9 @@ include("encabezado2.php"); //incluimos el archivo logotipo.php
 <!DOCTYPE html>
 <html lang="en">
 <body>
-<h3>Reservas</h3>
-<h3>aca se veran las reservas realizadas</h3>
-echo  $_SESSION['usuario']
+    <h3>Reservas</h3>
+    <h3>aca se veran las reservas realizadas</h3>
+      <p><?php echo  $_SESSION['usuario'];?></p>
+
 </body>
 </html>
