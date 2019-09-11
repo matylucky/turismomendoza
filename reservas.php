6d94a434301df1f3132543e24f06994502a870b2
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-11 11:31:24 -0300

    Update reservas.php

--- reservas.php
+++ reservas.php
@@ -1,53 +1,98 @@
 <?php
-include("encabezado2.php"); //incluimos el archivo logotipo.php
-/* include 'conexion.php';
-
-$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
- if ($conexion->connect_error) {
- die("La conexion falló: " . $conexion->connect_error);
+session_start();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+ 
+ 
+ 
+} else {
+   echo "Inicia Sesion para acceder a este contenido.<br>";
+   echo "<br><a href='login.html'>Login</a>";
+   echo "<br><br><a href='index.html'>Registrarme</a>";
+   header('Location: https://turismomendoza.herokuapp.com/login.html');//redirige a la página de login si el usuario quiere ingresar sin iniciar sesion
+exit;
 }
-$query = "SELECT * FROM $tbl_name2";
- if $row['RES_EMAIL'] == $_SESSION['mail']{
-     echo "Aca va a figurar reserva, matcheo de mails exitosa";
- }
-
- mysqli_close($conexion);
-    
-/*include("conexion.php");
-
-
-$link = mysql_connect($server, $db_user, $db_pass) 
-or die ("Could not connect to mysql because ".mysql_error()); 
-
-// select the database
-mysql_select_db($database) 
-or die ("Could not select database because ".mysql_error()); 
-
-$mail2 = $_POST['mail2'];
-
-$match = "Select * FROM $tbl_name2 WHERE RES_EMAIL = '$mail2'";
-
-$qry = mysql_query($match) 
-or die ("Could not match data because ".mysql_error()); 
-$num_rows = mysql_num_rows($qry); 
-
-if ($num_rows > 0) { 
-    
-        
-    $_SESSION['pasajero'] = $row['PAS_NOMBRE'];
-    $_SESSION['mail2'] = $row['RES_EMAIL'];
-    $_SESSION['dni2'] = $row['PAS_DNI'];
+//$usuarios = $_POST['usuarios'];
+ 
+$now = time();
+if($now > $_SESSION['expire']) {
+session_destroy();
+header('Location: https://turismomendoza.herokuapp.com/login.html');//redirige a la página de login, modifica la url a tu conveniencia
+echo "Tu sesion a expirado,
+<a href='login.html'>Inicia Sesion</a>";
+exit;
 }
-else{
-echo "Lo Sentimos no tiene reserva registrada"; 
-}
-    exit;
-
-} 
-ob_end_flush();
-
-*/
+//-----------------------------------------------
+/*$paquete2 = "";
+//	   for ($i=0;$i<count($paquete);$i++)    
+{     
+$paquete2 = "<br> paquete " . $i . ": " . $paquete[$i];    
+}*/ 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Registro de Pasajeros</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+    
+  .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none; 
+    }
+  }
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"> <p><?php echo  $_SESSION['usuario'];?></p> </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="#"><a href="#">Inicio</a></li>
+        <li><a href="#">Reservas</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="contacto2.php">Contacto</a></li>
+      </ul>
+             </button> <a class="navbar-brand navbar-right" href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Cerrar Sesión</a>
+      <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logear Usuario</a><onclick="location.href='index.html'"></li>
+      </ul> -->
+    </div>
+  </div>
+</nav>
+	
+	
+	
+<body>
 
 <!DOCTYPE html>
 <html lang="en">
