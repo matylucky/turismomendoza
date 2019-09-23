dee237ed4fa2e42513dc34d5b3fbbc284e76d38e
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-23 18:22:08 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,8 @@ de esta forma solo tendras que modificar un solo archivo -->
 <?php
 $host_db = "us-cdbr-iron-east-02.cleardb.net";
  $user_db = "bdaacf63d00d60";
- $pass_db = "565681cb";
+ //$pass_db = "565681cb";
+ $pass_db = "c1969fe7872181d";
  $db_name = "heroku_06e2145fb0a0577";
  $tbl_name = "usuario";
  $tbl_name2 = "reserva2";
