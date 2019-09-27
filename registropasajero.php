f7aa919cb2c233113c4a25800b33a65ae10104cd
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-27 10:55:37 -0300

    Update registropasajero.php

--- registropasajero.php
+++ registropasajero.php
@@ -131,18 +131,18 @@ $paquete2 = "<br> paquete " . $i . ": " . $paquete[$i];
     <div class="form-group">
       <label class="control-label col-sm-2" for="email">Email:</label>
       <div class="col-sm-10">
-        <input type="email" class="form-control" maxlength="50" id="email" name="email" value="<? echo  $_SESSION['mail'] ?>">
+        <input type="email" class="form-control" maxlength="50" id="email" name="email" placeholder="<? echo  $_SESSION['mail'] ?>">
       </div>
     </div>
-  <div class="form-group">
+<!--   <div class="form-group">
       <label class="control-label col-sm-2" for="nacionalidad">Nacionalidad</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" maxlength="25" placeholder="Ingresa tu nacionalidad" name="nacionalidad" required>
       </div>
-    </div>
+    </div>-->
 
 	
-   <!-- 
+   
     <div class="form-group">
     <label class="control-label col-sm-2" for="nacionalidad">Nacionalidad</label>
       <div class="col-sm-10">
@@ -384,7 +384,7 @@ $paquete2 = "<br> paquete " . $i . ": " . $paquete[$i];
 		<option value="ZW">Zimbabue</option>
 		</select>
         </div>
-    </div>-->
+    </div>
    <!-- <div class="form-group">
       <label class="control-label col-sm-2" for="dni">DNI:</label>
       <div class="col-sm-10">          
