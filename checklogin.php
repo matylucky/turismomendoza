202ba9a9c6cae7d3f48e507c20ff996758f2a7a9
matylucky <50973615+matylucky@users.noreply.github.com>
2019-09-27 09:12:02 -0300

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -44,7 +44,9 @@ if ($result->num_rows > 0) {     }
   $row = $result->fetch_array(MYSQLI_ASSOC);
  // if (password_verify($password, $row['password'])) { 
 //if($row['ROL_ID'] == "2"){
-if (mysql_num_rows($ROL_ID) == 2) {
+	
+if(mysql_num_rows[$ROL_ID] == "2"){
+//if (mysql_num_rows($ROL_ID) == 2) {
  //echo "Inicia Sesion para acceder a este contenido.<br>";
    echo "<br><a href='test.php'>Admin</a>";
    //echo "<br><br><a href='index.html'>Registrarme</a>";
