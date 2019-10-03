6d5299353b4c8537e2d755677e54c207b5eca000
matylucky <50973615+matylucky@users.noreply.github.com>
2019-10-03 10:55:16 -0300

    Update reporte-movil.php

--- reporte-movil.php
+++ reporte-movil.php
@@ -10,10 +10,10 @@
 <?php 
  // Conecta con el servidor de MySQL 
  $mysqli = @new mysqli( 
-        ’us-cdbr-iron-east-02.cleardb.net’,   // El servidor 
-        ’bdaacf63d00d60’,    // El usuario 
-         ’c1969fe7872181d’,          // La contraseña 
-         ’heroku_06e2145fb0a0577’); // La base de datos 
+        "us-cdbr-iron-east-02.cleardb.net",   // El servidor 
+        "bdaacf63d00d60",    // El usuario 
+         "c1969fe7872181d",          // La contraseña 
+         "heroku_06e2145fb0a0577"); // La base de datos 
  
  if($mysqli->connect_error) { 
    echo ’<p>Error al conectar con la base de datos: ’ . $mysqli->connect_error; 
@@ -29,20 +29,20 @@
    exit; 
  } 
  
- echo ’<table><tr>’; 
- echo ’<th>BUS_ID</th><th>BUS_PATENTE</th><th>BUS_CAPACIDAD</th>’; 
- echo ’<th>BUS_ESTADO</th>’; 
- echo ’</tr>’; 
+ echo "<table><tr>"; 
+ echo "<th>BUS_ID</th><th>BUS_PATENTE</th><th>BUS_CAPACIDAD</th>"; 
+ echo "<th>BUS_ESTADO</th>"; 
+ echo "</tr>"; 
  // Recorre el resultado y lo muestra en forma de tabla HTML 
  while($fila = $resultado->fetch_assoc()) { 
-   echo ’<tr>’; 
-   echo ’<td>’ . $fila[’BUS_ID’] . ’</td>’; 
-   echo ’<td>’ . $fila[’BUS_PATENTE’] . ’</td>’; 
-   echo ’<td>’ . $fila[’BUS_CAPACIDAD’] . ’</td>’; 
-   echo ’<td>’ . $fila[’BUS_ESTADO’] . ’</td>’; 
-   echo ’</tr>’; 
+   echo "<tr>"; 
+   echo "<td>" . $fila["BUS_ID"] . "</td>"; 
+   echo "<td>" . $fila["BUS_PATENTE"] . "</td>"; 
+   echo "<td>" . $fila["BUS_CAPACIDAD"] . "</td>"; 
+   echo "<td>" . $fila["BUS_ESTADO"] . "</td>"; 
+   echo "</tr>"; 
  } 
- echo ’</table>’; 
+ echo "</table>"; 
  
  // Libera la memoria ocupada por el resultado 
  $resultado->close(); 
