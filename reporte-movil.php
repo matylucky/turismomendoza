7f1eb26532d2e755bb832e43bd8f7599d8250a97
matylucky <50973615+matylucky@users.noreply.github.com>
2019-10-03 10:43:22 -0300

    Update reporte-movil.php

--- reporte-movil.php
+++ reporte-movil.php
@@ -4,55 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Reporte</title>
 </head>
 <body>
 <?php 
-  // Devuelve todas las filas de una consulta a una tabla de una base de datos 
-  // en forma de tabla de HTML 
-  function sql_dump_result($result) 
-  { 
-    $line = ''; 
-    $head = '';
-	
-  while($temp = mysql_fetch_assoc($result)) 
-  { 
-    if(empty($head)) 
-    { 
-      $keys = array_keys($temp); 
-      $head = '<tr><th>' . implode('</th><th>', $keys). '</th></tr>'; 
-    }
-	
-    $line .= '<tr><td>' . implode('</td><td>', $temp). '</td></tr>'; 
-  }
-  
-  return '<table>' . $head . $line . '</table>'; 
-}
-
-  // Se conecta al SGBD 
-  if(!($iden = mysql_connect("us-cdbr-iron-east-02.cleardb.net", "bdaacf63d00d60", "c1969fe7872181d"))) 
-    die("Error: No se pudo conectar");
-    
-    
-    // Selecciona la base de datos 
-  if(!mysql_select_db("heroku_06e2145fb0a0577", $iden))
-    die("Error: No existe la base de datos"); 
-	
-  // Sentencia SQL: muestra todo el contenido de la tabla "books" 
-  $sentencia = "SELECT * FROM buses"; 
-  // Ejecuta la sentencia SQL 
-  $resultado = mysql_query($sentencia, $iden); 
-  if(!$resultado) 
-    die("Error: no se pudo realizar la consulta");
-
-  // Muestra el contenido de la tabla como una tabla HTML	
-  echo sql_dump_result($resultado); 
-  
-  // Libera la memoria del resultado
-  mysql_free_result($resultado);
-
-  // Cierra la conexión con la base de datos 
-  mysql_close($iden); 
-?> 	
-</body>
+ // Conecta con el servidor de MySQL 
+ $mysqli = @new mysqli( 
+        ’us-cdbr-iron-east-02.cleardb.net’,   // El servidor 
+        ’bdaacf63d00d60’,    // El usuario 
+         ’c1969fe7872181d’,          // La contraseña 
+         heroku_06e2145fb0a0577); // La base de datos 
+ 
+ if($mysqli->connect_errno) { 
+   echo ’<p>Error al conectar con la base de datos: ’ . $mysqli->connect_error; 
+   echo ’</p>’; 
+   exit; 
+ } 
+ 
+ // Ejecuta una sentencia SQL 
+ $sentencia = ’SELECT * FROM buses; 
+ if(!($resultado = $mysqli->query($sentencia))) { 
+   echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error; 
+   echo ’</p>’; 
+   exit; 
+ } 
+ 
+ echo ’<table><tr>’; 
+ echo ’<th>BUS_ID</th><th>BUS_PATENTE</th><th>BUS_CAPACIDAD</th>’; 
+ echo ’<th>BUS_ESTADO</th>’; 
+ echo ’</tr>’; 
+ // Recorre el resultado y lo muestra en forma de tabla HTML 
+ while($fila = $resultado->fetch_assoc()) { 
+   echo ’<tr>’; 
+   echo ’<td>’ . $fila[’BUS_ID’] . ’</td>’; 
+   echo ’<td>’ . $fila[’BUS_PATENTE’] . ’</td>’; 
+   echo ’<td>’ . $fila[’BUS_CAPACIDAD’] . ’</td>’; 
+   echo ’<td>’ . $fila[’BUS_ESTADO’] . ’</td>’; 
+   echo ’</tr>’; 
+ } 
+ echo ’</table>’; 
+ 
+ // Libera la memoria ocupada por el resultado 
+ $resultado->close(); 
+ // Cierra la conexión 
+ $mysqli->close(); 
+?> 
+</body> 
 </html>
+
+
