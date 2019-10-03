396f5e19271489587112ef6f1c062a4dd340a200
matylucky <50973615+matylucky@users.noreply.github.com>
2019-10-03 11:10:34 -0300

    Update reporte-movil.php

--- reporte-movil.php
+++ reporte-movil.php
@@ -9,32 +9,29 @@
 <body>
 <?php 
  // Conecta con el servidor de MySQL 
- $mysqli = @new mysqli( 
-        "us-cdbr-iron-east-02.cleardb.net",   // El servidor 
-        "bdaacf63d00d60",    // El usuario 
-         "c1969fe7872181d",          // La contraseña 
-         "heroku_06e2145fb0a0577"); // La base de datos 
- 
- if($mysqli->connect_error) { 
-   echo ’<p>Error al conectar con la base de datos: ’ . $mysqli->connect_error; 
-   echo ’</p>’; 
-   exit; 
- } 
+ include 'conexion.php';
+ $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+ if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
  
  // Ejecuta una sentencia SQL 
- $sentencia = "SELECT * FROM buses"; 
- if(!($resultado = $mysqli->query($sentencia))) { 
-   echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error; 
+ $consulta = "SELECT * FROM $tbl_buses"; 
+ /*if(!($resultado = $conexion->query($consulta))) { 
+   echo "<p>Error al ejecutar la sentencia <b>$consulta</b>: " . $conexion->error; 
    echo ’</p>’; 
    exit; 
- } 
- 
+ } */
+ $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+    
+    
  echo "<table><tr>"; 
  echo "<th>BUS_ID</th><th>BUS_PATENTE</th><th>BUS_CAPACIDAD</th>"; 
  echo "<th>BUS_ESTADO</th>"; 
  echo "</tr>"; 
  // Recorre el resultado y lo muestra en forma de tabla HTML 
- while($fila = $resultado->ffetch_array(MYSQLI_ASSOC)) { 
+ //while($fila = $resultado->ffetch_array(MYSQLI_ASSOC)) { 
+ while ($columna = mysqli_fetch_array( $resultado )) {
    echo "<tr>"; 
    echo "<td>" . $fila["BUS_ID"] . "</td>"; 
    echo "<td>" . $fila["BUS_PATENTE"] . "</td>"; 
@@ -47,7 +44,7 @@
  // Libera la memoria ocupada por el resultado 
  $resultado->close(); 
  // Cierra la conexión 
- $mysqli->close(); 
+ $conexion->close(); 
 ?> 
 </body> 
 </html>
