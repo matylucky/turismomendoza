22e9c4d9d7dd0701b926d406c9f99606c2fa952e
matylucky <50973615+matylucky@users.noreply.github.com>
2019-10-10 12:45:45 -0300

    Update viajes2.php

--- viajes2.php
+++ viajes2.php
@@ -117,7 +117,9 @@ exit;
  } */
  $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
     
-    
+ $columna = mysqli_fetch_array( $resultado ))
+ 
+ $prueba['prueba'] =  $columna["PAS_NOMBRE"];   
  /*echo "<table borde='2'>";
  echo "<tr>";
  echo "<th>RES_ID</th><th>PAS_NOMBRE</th><th>PAS_DNI</th>"; 
