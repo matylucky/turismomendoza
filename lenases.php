ad44e64ac3eac01f363b5df683a80b5572fc3505
matylucky <50973615+matylucky@users.noreply.github.com>
2019-10-25 08:03:18 -0300

    Update lenases.php

--- lenases.php
+++ lenases.php
@@ -16,15 +16,15 @@
  $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
     
     
- echo "<table border='2' style='padding:10px;background-color:#FFFFFF;border-collapse:collapse;border:2px solid #6699FF;color:#000000;width:400'>";
- echo "<tr style='background-color:#4CAF50;color:white'>";
+ echo "<table border='2' style='background-color:#FFFFFF;border-collapse:collapse;border:2px solid #6699FF;color:#000000;width:400'>";
+ echo "<tr style='background-color:#4CAF50;color:white;padding:10px'>";
  echo "<th>N° reserva</th><th>Nombre Pasajaero</th><th>DNI</th>"; 
  echo "<th>Mail de usuario</th><th>Destino</th><th>Fecha</th>"; 
  echo "</tr>"; 
  // Recorre el resultado y lo muestra en forma de tabla HTML 
  //while($fila = $resultado->ffetch_array(MYSQLI_ASSOC)) { 
  while ($columna = mysqli_fetch_array( $resultado )) {
-   echo "<tr>"; 
+   echo "<tr style='padding:10px'>"; 
    echo "<td>" . $columna["RES_ID"] . "</td>";
    echo "<td>" . $columna["PAS_NOMBRE"] . "</td>"; 
    echo "<td>" . $columna["PAS_DNI"] . "</td>"; 
