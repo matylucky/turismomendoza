ceaa2330b194b223956bb3e88b71c2c8cffd3ff2
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-18 23:14:22 -0300

    Update getfechas.php

--- getfechas.php
+++ getfechas.php
@@ -3,7 +3,8 @@
     
 	$DES_ID = $_POST['DES_ID'];
 	
-	$queryM = "SELECT PAQ_FECHA FROM paquetes WHERE DES_ID = '$DES_ID' ORDER BY PAQ_FECHA";
+	//$queryM = "SELECT PAQ_FECHA FROM paquetes WHERE DES_ID = '$DES_ID' ORDER BY PAQ_FECHA";
+	$queryM = "SELECT PAQ_FECHA FROM paquetes WHERE DES_ID = '$DES_ID'";	
 	$resultadoM = $mysqli->query($queryM);
 	
 	$html= "<option value='0'>Seleccionar fecha</option>";
