b2bf70b0d0bc207591b13eb950909515bcc7969b
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-19 11:07:28 -0300

    Update viajes3.php

--- viajes3.php
+++ viajes3.php
@@ -163,9 +163,50 @@ foreach($fila AS $key => $value) { $fila[$key] = stripslashes($value); } ?>
 <? } ?>
 </table>
 </div>
-<br />
+<br /><br /><br /><br /><br /><br />
 
 
+ <?php 
+ // Conecta con el servidor de MySQL 
+ include 'conexion.php';
+ $conexion = mysqli_connect($host_db, $user_db, $pass_db, $db_name);
+ if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+ 
+ // Ejecuta una sentencia SQL 
+ $consulta = "SELECT * FROM $tbl_name2"; 
+ /*if(!($resultado = $conexion->query($consulta))) { 
+   echo "<p>Error al ejecutar la sentencia <b>$consulta</b>: " . $conexion->error; 
+   echo ’</p>’; 
+   exit; 
+ } */
+ $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+  
+ echo "<table borde='2'>";
+ echo "<tr>";
+ echo "<th>RES_ID</th><th>PAS_NOMBRE</th><th>PAS_DNI</th>"; 
+ echo "<th>RES_MAIL</th><th>PAQ_ID</th><th>PAQ_FECHA</th>"; 
+ echo "</tr>"; 
+ // Recorre el resultado y lo muestra en forma de tabla HTML 
+ //while($fila = $resultado->ffetch_array(MYSQLI_ASSOC)) { 
+ while ($columna = mysqli_fetch_array( $resultado )) {
+   echo "<tr>"; 
+   echo "<td>" . $columna["RES_ID"] . "</td>"; 
+   echo "<td>" . $columna["PAS_NOMBRE"] . "</td>"; 
+   echo "<td>" . $columna["PAS_DNI"] . "</td>"; 
+   echo "<td>" . $columna["RES_MAIL"] . "</td>"; 
+   echo "<td>" . $columna["PAQ_ID"] . "</td>"; 
+   echo "<td>" . $columna["PAQ_FECHA"] . "</td>"; 
+   echo "</tr>"; 
+ } 
+ echo "</table>";
+ 
+ // Libera la memoria ocupada por el resultado 
+ $resultado->close(); 
+ // Cierra la conexión 
+ $conexion->close(); 
+?> 
 
 
     
