80c6aa5e136bdae7a7397065a74538326030cf01
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-21 09:35:12 -0300

    Update getfechas2.php

--- getfechas2.php
+++ getfechas2.php
@@ -3,11 +3,14 @@
 //Recibo id de vehiculo
 	 $mysqli = new mysqli("us-cdbr-iron-east-02.cleardb.net", "bdaacf63d00d60", "c1969fe7872181d", "heroku_06e2145fb0a0577");
 
-$id = $_GET['DEST'];
+$id = $_POST['destino'];
 //Consulto la tabla reserva2 por id de vehiculo
 $sqltipo="SELECT * FROM reserva2 where PAQ_ID=".$id;
 $consultatipo = mysqli_query($sqltipo, $mysqli);
 //devuelvo el resultado
 while($filatipo = mysqli_fetch_array($consultatipo)){ 
-			echo "<option>" . $filatipo['PAQ_FECHA'] . "</option>";
-} ?>
+	$html.= "<option value='". $filatipo['PAQ_FECHA'] ."'>". $filatipo['PAQ_FECHA'] ."</option>";
+			//echo "<option>" . $filatipo['PAQ_FECHA'] . "</option>";
+} 
+echo $html;
+?>
