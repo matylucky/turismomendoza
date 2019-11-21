39a5a7ffb3c4c26490ed029efc923d10c0ea2003
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-21 08:31:02 -0300

    Update viajes3.php

--- viajes3.php
+++ viajes3.php
@@ -40,15 +40,16 @@ include 'conexion.php';
   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script>
 	$(function(){
-		$("#destino").change(function(){ // se activa el script cuando selecciono el select vehiculo
-			 destin=$(this).val() // Tomo el valor seleccionado
+		$("#destino").on('change',function(){ // se activa el script cuando selecciono el select vehiculo
+			alert("se ha seleccionado");
+			/*destin=$(this).val() // Tomo el valor seleccionado
 
 			 //envio a una pagina que hara la consulta sql y me devolvera los datos para poner en el select
 
 			 $.get("getfechas2.php?PAQ_ID="+destin,
 				 function(data){
 					 $("#fecha").html(data); // Tomo el resultado e inserto los datos en el select marca	
-				 });															
+				 });*/															
 		});
 		});	
   </script>
