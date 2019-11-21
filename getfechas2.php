b57900d2f2c99a30df3ce99a051f2f5911ff78e5
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-21 07:00:52 -0300

    Update getfechas2.php

--- getfechas2.php
+++ getfechas2.php
@@ -6,5 +6,5 @@ $sqltipo="SELECT * FROM reserva2 where PAQ_ID=".$id;
 $consultatipo = mysql_query($sqltipo);
 //devuelvo el resultado
 while($fila = mysql_fetch_array($consultatipo)){ ?>
-			 <option value="<? echo $filatipo['PAQ_FECHA2'] ?>"><? echo $filatipo['PAQ_FECHA2'] ?></option>
+			 <option value="<? echo $fila['PAQ_FECHA2'] ?>"><? echo $fila['PAQ_FECHA2'] ?></option>
 <? } ?>
