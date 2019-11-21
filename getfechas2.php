f667b6f7ef68ad04fc41134b76a0847b7272573a
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-21 09:00:21 -0300

    Update getfechas2.php

--- getfechas2.php
+++ getfechas2.php
@@ -3,7 +3,7 @@
 //Recibo id de vehiculo
 	 $mysqli = new mysqli("us-cdbr-iron-east-02.cleardb.net", "bdaacf63d00d60", "c1969fe7872181d", "heroku_06e2145fb0a0577");
 
-$id = $_GET['PAQ_ID'];
+$id = $_GET['DEST'];
 //Consulto la tabla reserva2 por id de vehiculo
 $sqltipo="SELECT * FROM reserva2 where PAQ_ID=".$id;
 $consultatipo = mysql_query($sqltipo);
