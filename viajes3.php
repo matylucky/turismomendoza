644c77efaee3c5d94b679f152efc7ad57314faaa
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-21 08:34:40 -0300

    Update viajes3.php

--- viajes3.php
+++ viajes3.php
@@ -41,15 +41,15 @@ include 'conexion.php';
   <script>
 	$(function(){
 		$("#destino").on('change',function(){ // se activa el script cuando selecciono el select vehiculo
-			alert("se ha seleccionado");
-			/*destin=$(this).val() // Tomo el valor seleccionado
+			//alert("se ha seleccionado");
+			destin=$(this).val(); // Tomo el valor seleccionado
 
 			 //envio a una pagina que hara la consulta sql y me devolvera los datos para poner en el select
 
 			 $.get("getfechas2.php?PAQ_ID="+destin,
 				 function(data){
 					 $("#fecha").html(data); // Tomo el resultado e inserto los datos en el select marca	
-				 });*/															
+				 });															
 		});
 		});	
   </script>
