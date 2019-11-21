8dad5a4b746f624cd76d725ef02efb89b7a50840
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-21 09:50:15 -0300

    Update getfechas2.php

--- getfechas2.php
+++ getfechas2.php
@@ -5,11 +5,27 @@
 
 $id = $_POST['destino'];
 //Consulto la tabla reserva2 por id de vehiculo
-$sqltipo="SELECT * FROM reserva2 where PAQ_ID=".$id;
-$consultatipo = mysqli_query($sqltipo, $mysqli);
+/*
+	$queryM = "SELECT id_municipio, municipio FROM t_municipio WHERE id_estado = '$id_estado' ORDER BY municipio";
+	$resultadoM = $mysqli->query($queryM);
+	
+	$html= "<option value='0'>Seleccionar Municipio</option>";
+	
+	while($rowM = $resultadoM->fetch_assoc())
+	{
+		$html.= "<option value='".$rowM['id_municipio']."'>".$rowM['municipio']."</option>";
+	}
+	
+	echo $html;
+*/
+
+$html= "<option value='0'>Seleccionar</option>";
+
+$sqltipo="SELECT * FROM reserva2 where PAQ_ID= '$id'";
+$consultatipo = $mysqli->query($sqltipo);
 //devuelvo el resultado
-while($filatipo = mysqli_fetch_array($consultatipo)){ 
-	$html.= "<option value='". $filatipo['PAQ_FECHA'] ."'>". $filatipo['PAQ_FECHA'] ."</option>";
+while($filatipo = $consultatipo->fetch_array()){ 
+	$html.= "<option value='". $filatipo['PAQ_FECHA2'] ."'>". $filatipo['PAQ_FECHA2'] ."</option>";
 			//echo "<option>" . $filatipo['PAQ_FECHA'] . "</option>";
 } 
 echo $html;
