122d7f2a14d78fd5ba989a31ab68eb4f3f6966b1
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-22 10:07:42 -0300

    Update viajes3.php

--- viajes3.php
+++ viajes3.php
@@ -182,74 +182,8 @@ include 'conexion.php';
 	    </form>
 
 	</div>
-<div id=”listado2” name="listado2">
-	  <div class="jumbotron">
-		  
-            	<table width="615" border=1 cellpadding="3" cellspacing="0" > 
-		 <tr><td><b>N° Reserva</b></td>
-		 <td><b>Nombre Pasajero</b></td>
-		 <td><b>DNI</b></td>
-		 <td><b>Mail de usuario</b></td>
-		 <td><b>Destino</b></td>
-		 <td><b>Fecha</b></td>
-		 <td><b>Fecha2</b></td>
-		</tr>  
-			
-		 <?php 
-			
 
-		  //$mail = $_SESSION['mail'];
-
-		 /*print("el usuario es : $mail");
-		 print("");*/
-		 // Conecta con el servidor de MySQL 
-		 /*include 'conexion.php';
-		 $conexion = mysqli_connect($host_db, $user_db, $pass_db, $db_name);
-		 if ($conexion->connect_error) {
-		 die("La conexion falló: " . $conexion->connect_error);
-		}*/
-			 $conexion = new mysqli("us-cdbr-iron-east-02.cleardb.net", "bdaacf63d00d60", "c1969fe7872181d", "heroku_06e2145fb0a0577");
-		//$destino = $_POST['destino'];
-		//$fecha = $_POST['fecha'];
-		 // Ejecuta una sentencia SQL 
-		 //$consulta = "SELECT * FROM $tbl_name2"; 
-		 //$consulta = "SELECT * FROM reserva2 WHERE PAQ_FECHA2='.$_GET["fecha"]' ";
-		 $consulta = "SELECT * FROM reserva2";
-		 /*if(!($resultado = $conexion->query($consulta))) { 
-		   echo "<p>Error al ejecutar la sentencia <b>$consulta</b>: " . $conexion->error; 
-		   echo ’</p>’; 
-		   exit; 
-		 } */
-		// $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-		$resultado = $mysqli->query($consulta);
-
-	 
-		 //echo "<table border='2' style='background-color:#FFFFFF;border-collapse:separate;border:2px solid #6699FF;color:#000000'>";
-		 //echo "<tr style='background-color:#4CAF50;color:white'>";
-		 //echo "<th>N° reserva</th><th>Nombre Pasajaero</th><th>DNI</th>"; 
-		 //echo "<th>Destino</th><th>Fecha</th><th>Fecha2</th>"; 
-		 //echo "</tr>"; 
-		 // Recorre el resultado y lo muestra en forma de tabla HTML 
-		 //while($fila = $resultado->ffetch_array(MYSQLI_ASSOC)) { 
-		 while ($columna = mysqli_fetch_array( $resultado )) {
-		   echo "<tr>"; 
-		   echo "<td>" . $columna["RES_ID"] . "</td>"; 
-		   echo "<td>" . $columna["PAS_NOMBRE"] . "</td>"; 
-		   echo "<td>" . $columna["PAS_DNI"] . "</td>"; 
-		   echo "<td>" . $columna["RES_MAIL"] . "</td>"; 
-		   echo "<td>" . $columna["PAQ_ID"] . "</td>"; 
-		   echo "<td>" . $columna["PAQ_FECHA"] . "</td>"; 
-		   echo "<td>" . $columna["PAQ_FECHA2"] . "</td>"; 
-		   echo "</tr>"; 
-		 } 
-		 echo "</table>"; 
-
-
-		?> 
-         
-        </div>
-</div>	
-	
+	  
 <div id=”listado”>
 <table width="615" border=1 cellpadding="3" cellspacing="0" >
 <tr><td><b>N° Reserva</b></td>
