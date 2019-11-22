43125d9cfa60cf51265e962d98844f0f1a15922b
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-22 08:36:00 -0300

    Update repoviajes.php

--- repoviajes.php
+++ repoviajes.php
@@ -5,26 +5,39 @@
  /*print("el usuario es : $mail");
  print("");*/
  // Conecta con el servidor de MySQL 
- include 'conexion.php';
+ /*include 'conexion.php';
  $conexion = mysqli_connect($host_db, $user_db, $pass_db, $db_name);
  if ($conexion->connect_error) {
  die("La conexion falló: " . $conexion->connect_error);
-}
+}*/
+
+	 $conexion = new mysqli("us-cdbr-iron-east-02.cleardb.net", "bdaacf63d00d60", "c1969fe7872181d", "heroku_06e2145fb0a0577");
+
 $destino = $_POST['destino'];
 $fecha = $_POST['fecha'];
 
 
  // Ejecuta una sentencia SQL 
  //$consulta = "SELECT * FROM $tbl_name2"; 
- $consulta = "SELECT * FROM $tbl_name2 WHERE PAQ_FECHA2='".$_POST['fecha']."' ";
+ $consulta = "SELECT * FROM reserva2 WHERE PAQ_FECHA2='".$_POST['fecha']."' ";
  /*if(!($resultado = $conexion->query($consulta))) { 
    echo "<p>Error al ejecutar la sentencia <b>$consulta</b>: " . $conexion->error; 
    echo ’</p>’; 
    exit; 
  } */
- $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-    
-    
+// $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+ $resultado = $mysqli->query($consulta);
+   
+  echo "  <table width="615" border=1 cellpadding="3" cellspacing="0" >"; 
+ echo "<tr><td><b>N° Reserva</b></td>"; 
+ echo "<td><b>Nombre Pasajero</b></td>"; 
+ echo "<td><b>DNI</b></td>"; 
+ echo "<td><b>Mail de usuario</b></td>"; 
+ echo "<td><b>Destino</b></td>"; 
+ echo "<td><b>Fecha</b></td>"; 
+ echo "<td><b>Fecha2</b></td>"; 
+
+echo "</tr>  "; 
  //echo "<table border='2' style='background-color:#FFFFFF;border-collapse:separate;border:2px solid #6699FF;color:#000000'>";
  //echo "<tr style='background-color:#4CAF50;color:white'>";
  //echo "<th>N° reserva</th><th>Nombre Pasajaero</th><th>DNI</th>"; 
