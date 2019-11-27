66096ee234330f0531ef56a6c3798e515156ec53
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-27 15:01:40 -0300

    Create modestado-destino.php

---
+++ modestado-destino.php
@@ -0,0 +1,21 @@
+<?php
+include 'conexion.php';
+ 
+ $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+ if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+ 
+//$query = "INSERT INTO $tbl_paquetes (PAQ_ESTADO) VALUES (1)";
+$query = "UPDATE $tbl_destinos  SET DES_ESTADO='1' WHERE DES_NOMBRE='$_POST[des]'";
+ if ($conexion->query($query) === TRUE) {
+ 
+ echo "<h3>" . "Se realizo la deshabilitación del paquete: " . $_SESSION['usuario'] . "</h3>" . "\n\n";
+ echo "<h3>" .  "<a href='paquetes.php'>Inicio</a>" . "</h3>"; 
+ 
+ }
+ else {
+ echo "Error al crear una habiltiación." . $query . "<br>" . $conexion->error; 
+   }
+ mysqli_close($conexion);
+?>
