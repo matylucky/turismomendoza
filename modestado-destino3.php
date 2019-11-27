825b46f7079708730e66fd7639f8d1bf15799402
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-27 15:31:19 -0300

    Create modestado-destino3.php

---
+++ modestado-destino3.php
@@ -0,0 +1,21 @@
+<?php
+include 'conexion.php';
+ 
+ $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+ if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+ 
+//$query = "INSERT INTO $tbl_paquetes (PAQ_ESTADO) VALUES (1)";
+$query = "UPDATE $tbl_destinos  SET DES_ESTADO='0' WHERE DES_NOMBRE='$_POST[des]'";
+ if ($conexion->query($query) === TRUE) {
+ 
+ echo "<h3>" . "Se realizo la deshabilitación del destino: " . $_SESSION['usuario'] . "</h3>" . "\n\n";
+ echo "<h3>" .  "<a href='destinos.php'>Inicio</a>" . "</h3>"; 
+ 
+ }
+ else {
+ echo "Error al crear una deshabiltiación." . $query . "<br>" . $conexion->error; 
+   }
+ mysqli_close($conexion);
+?>
