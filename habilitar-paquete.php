dfc9fba7165ecb7f6c9dd8e30ca47a45faa750ad
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-27 13:24:02 -0300

    Create habilitar-paquete.php

---
+++ habilitar-paquete.php
@@ -0,0 +1,20 @@
+<?php
+include 'conexion.php';
+ 
+ $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+ if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+ 
+$query = "UPDATE INTO $tbl_paquetes (PAQ_ESTADO) VALUES (1)";
+ if ($conexion->query($query) === TRUE) {
+ 
+ echo "<h3>" . "Se realizo la habilitación del paquete: " . $_SESSION['usuario'] . "</h3>" . "\n\n";
+ echo "<h3>" .  "<a href='paquetes.php'>Inicio</a>" . "</h3>"; 
+ 
+ }
+ else {
+ echo "Error al crear un movil." . $query . "<br>" . $conexion->error; 
+   }
+ mysqli_close($conexion);
+?>
