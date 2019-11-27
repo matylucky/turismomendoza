4fe687e185a977c6c6234cfff0d412dc10505f17
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-27 13:30:09 -0300

    Update habilitar-paquete.php

--- habilitar-paquete.php
+++ habilitar-paquete.php
@@ -6,7 +6,7 @@ include 'conexion.php';
  die("La conexion falló: " . $conexion->connect_error);
 }
  
-$query = "UPDATE INTO $tbl_paquetes (PAQ_ESTADO) VALUES ("1")";
+$query = "INSERT INTO $tbl_paquetes (PAQ_ESTADO) VALUES ("1")";
  if ($conexion->query($query) === TRUE) {
  
  echo "<h3>" . "Se realizo la habilitación del paquete: " . $_SESSION['usuario'] . "</h3>" . "\n\n";
