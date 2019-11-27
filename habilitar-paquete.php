9f3ca17c72005f962868608fc84f52c89894bff7
matylucky <50973615+matylucky@users.noreply.github.com>
2019-11-27 13:57:58 -0300

    Update habilitar-paquete.php

--- habilitar-paquete.php
+++ habilitar-paquete.php
@@ -7,7 +7,7 @@ include 'conexion.php';
 }
  
 //$query = "INSERT INTO $tbl_paquetes (PAQ_ESTADO) VALUES (1)";
-$query = "UPDATE $tbl_paquetes SET PAQ_ESTADO='1' WHERE DES_ID='$_POST[habilita]'";
+$query = "UPDATE $tbl_paquetes SET PAQ_ESTADO='1' WHERE DES_ID='$_POST[habilita]' AND PAQ_ESTADO='0'";
 
  if ($conexion->query($query) === TRUE) {
  
